a5eecf6f305b9bb19fcbabf18b2fc18832f06e4c
ArslanYM <arsalanyaqmalik@gmail.com>
2023-07-28 22:17:25 +0530

    fix: fixed some bugs and links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,9 @@ export const Footer = () => {
           <span className="ml-3 text-xl">Dev Leaders</span>
         </a>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
-          © 2023 Dev Leaders —
+          © 2023 First Steps —
           <a
-            href=""
+            href="https://twitter.com/ArsalanYmalik"
             className="text-gray-500 ml-1"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,19 +29,10 @@ export const Footer = () => {
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-400">
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </a>
-          <a className="ml-3 text-gray-400">
+          <a
+            href="https://twitter.com/ArsalanYmalik"
+            className="ml-3 text-gray-400 hover:text-white "
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -53,21 +44,7 @@ export const Footer = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className="ml-3 text-gray-400">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-            </svg>
-          </a>
-          <a className="ml-3 text-gray-400">
+          <a className="ml-3 text-gray-400 hover:text-white">
             <svg
               fill="currentColor"
               stroke="currentColor"
